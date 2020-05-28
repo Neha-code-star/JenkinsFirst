abbc9d6b5594884d274718cafbe9f4daae737027
neha <neha@LAPTOP-VLS79GSJ>
2020-05-28 17:20:29 -0500

    For jenkins

---
+++ MidSpring2020/src/BasicJava/DataType.java
@@ -0,0 +1,25 @@
+package BasicJava;
+
+public class DataType {
+	
+	public static void main(String[] args) {
+		
+		String name = "Neha";
+		String x = "20";
+		
+		System.out.println(name);
+		System.out.println(x);
+		System.out.println("I am "+name+" and I am "+x);
+		
+		second();
+	}
+	
+	public static void second() {
+		int x = 10;
+		int y = 86;
+		boolean b = true;
+		System.out.println(x+y);
+		System.out.println(b);
+	}
+
+}
---
+++ MidSpring2020/src/BasicJava/Print.java
@@ -0,0 +1,17 @@
+package BasicJava;
+
+public class Print {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Neha");
+		System.out.println("I'm from texas");
+
+		age();
+	}
+	
+	public static void age() {
+		System.out.println("I am 20 years old");
+	}
+
+}
